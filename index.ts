c1055d288622c8f0f440d739bdcccd25c7025f1f
Guaru34 <19170095@uttcampus.edu.mx>
2025-09-10 23:29:45 -0600

    Add API routing setup with Express

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { router } from './src/routes/routes'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -7,6 +8,7 @@ app.use(express.json())
 app.get('/', (_req, res) => {
     res.json({ message: 'api is healthy' })
 })
+app.use("/api", router)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
