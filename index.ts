0e57b1a61a7d299182958920b628b506045cc217
Guaru34 <19170095@uttcampus.edu.mx>
2025-09-10 23:20:16 -0600

    Add JSON body parsing middleware to Express app

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
 app.get('/', (_req, res) => {
     res.json({ message: 'api is healthy' })
 })
