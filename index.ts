566589a1a00c97e5cd35c2fedf53d15888edbc41
abel.dominguez <abel.dominguez@transmontes.com>
2025-10-08 13:18:10 -0600

    Add request logging middleware and integrate error handler

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 import { router } from './src/routes/routes'
+import { errorHandler } from './src/middlewares/handle-error'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use((req, _res, next) => {
+    console.log('Petición entrante: body', req.body)
+    console.log('Petición entrante: params', req.params)
+
+    next()
+})
 app.get('/', (_req, res) => {
     res.json({ message: 'api is healthy' })
 })
 app.use("/api", router)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
