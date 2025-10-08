20be485e948d95febc95c22ddb3b4b2762dafff0
abel.dominguez <abel.dominguez@transmontes.com>
2025-10-08 12:54:54 -0600

    Add authentication middleware to verify JWT tokens

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { prisma } from "../utils/prisma";
+import jwt from "jsonwebtoken";
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "No cabecera" })
+    }
+
+    const token = authHeader.replace("Bearer ", "")
+
+    try {
+        const tokenExists = await prisma.tokens.findFirst({
+            where: {
+                token,
+                active: true
+            }
+        })
+
+        if (!tokenExists) {
+            return res.status(401).json({ error: "Token invalido" })
+        }
+
+        if (tokenExists.expires_at < new Date()) {
+            return res.status(401).json({ error: "Token expirado" })
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET || "")
+        next()
+    } catch (error) {
+        next(error)
+    }
+}   
