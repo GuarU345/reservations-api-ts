2eb19c2b765c31937d18d873a12065021a47cd36
abel.dominguez <abel.dominguez@transmontes.com>
2025-10-08 16:15:33 -0600

    Set up initial project structure with Express server and TypeScript configuration

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { router } from './routes/routes'
+import { errorHandler } from './middlewares/handle-error'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use((req, _res, next) => {
+    console.log('Petición entrante: body', req.body)
+    console.log('Petición entrante: params', req.params)
+
+    next()
+})
+app.get('/', (_req, res) => {
+    res.json({ message: 'api is healthy' })
+})
+app.use("/api", router)
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
