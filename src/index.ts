88a3f9184bb91122b89a305df91d58acfa627107
abel.dominguez <abel.dominguez@transmontes.com>
2025-10-13 16:31:49 -0600

    Add CORS middleware to enable cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes/routes'
 import { errorHandler } from './middlewares/handle-error'
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use((req, _res, next) => {
     console.log('Petición entrante: body', req.body)
